429d1914c0e9cf4b85288e702933bc6980a8706c
Isaac Kim <kisaac95@yahoo.com>
2022-08-05 13:54:50 -0700

    v1.5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.prompt('Salutations')
+window.prompt('May we play a game of Yes and No?')
 var firstAnswer = window.prompt('Do you believe in Darwin?')
-console.log(firstAnswer === 'Yes')
 if (firstAnswer === 'Yes') {
     let secondAnswer = window.prompt('Do you believe in progress?') 
     if (secondAnswer === 'Yes') {
@@ -9,11 +9,32 @@ if (firstAnswer === 'Yes') {
             let fourthAnswer = window.alert('Then I suppose congratulations are due as it seems you have overcome the conundrum of living trials.')
         } else if (thirdAnswer === 'No') {
             let fourthAnswer = window.prompt('It would seem no organic entity does. Could you if it were unilaterally for the better?')
-        }
+         switch (fourthAnswer) {
+            case "Yes":
+                window.alert('HAHAHHAHAHAH I wonder');
+                break;
+            case "No":
+                window.alert('Indeed that seems very human of you');
+            break;
+            default:
+                window.alert('Shut your corny ass up you weak licentious blight')
+        }}        
     } else if (secondAnswer === 'No') {
         let thirdAnswer = window.prompt('Do you believe in Entropy?')
         if (thirdAnswer ==='Yes') {
             let fourthAnswer = window.prompt('Indeed. It seems inevitable that impermanence is the nature of existence. Do you welcome your end?')
+            switch (fourthAnswer) {
+                case "Yes":
+                    window.alert('HAHAHHAHAHAH I wonder');
+                    break;
+                case "No":
+                    window.alert('Indeed that seems very human of you');
+                break;
+                default:
+                    window.alert('Shut your corny ass up you weak licentious blight')
+            } 
+        } else if(thirdAnswer == "No") {
+        let thirdAnswer = window.alert('How blissfully ignorant of you')
         }
     }
 } else if (firstAnswer === 'No') {
@@ -22,5 +43,21 @@ if (firstAnswer === 'Yes') {
         let thirdAnswer = window.alert('How blissfully ignorant of you')
     } else if (secondAnswer === 'Yes') { 
         let thirdAnswer = window.prompt('Indeed. It seems inevitable that impermanence is the nature of existence. Do you welcome your end?')
+        if (thirdAnswer == 'Yes') {
+            let fourthAnswer = window.alert ('Then I suppose congratulations are due as it seems you have overcome the conundrum of living trials.')
+        } else if (thirdAnswer == 'No') {
+            let fourthAnswer = window.prompt('It would seem no organic entity does. Could you if it were unilaterally for the better?')
+         switch (fourthAnswer) {
+            case "Yes":
+                window.alert('HAHAHHAHAHAH I wonder');
+                break;
+            case "No":
+                window.alert('Indeed that seems very human of you');
+            break;
+            default:
+                window.alert('Shut your corny ass up you weak licentious blight')
+        }
+    }    
 }
 } 
+window.alert('If only you knew')
